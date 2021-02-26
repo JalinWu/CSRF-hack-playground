f492a8e475663d31e3c8a6f52545a646713d7fca
lilywu <Lily.Wu@auo.com>
2021-02-26 17:01:49 +0800

    My first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# CSRF-playground
+
+It's a teaching material for practicing CSRF, which is based on Node.js. Have fun and enjoy it.
+
+## Usage
+
+```sh
+$ npm install
+```
+
+```sh
+# Run with Nodemon
+$ npm run dev
+
+# Visit http://localhost:3000
+```
---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+
+const Balance = require('./models/Balance');
+
+const app = express();
+
+// Passport Config
+require('./config/passport')(passport);
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db + '/CSRF',
+    { useNewUrlParser: true ,useUnifiedTopology: true}
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// function dbInit() {
+//   Balance.deleteMany({}, () => {
+//     let newBalance = new Balance({
+//       name: 'Admin',
+//       balance: 5000
+//     })
+  
+//     newBalance.save();
+//   })
+// }
+
+// dbInit();
+
+// Routes
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, console.log(`Server running on  ${PORT}`));
